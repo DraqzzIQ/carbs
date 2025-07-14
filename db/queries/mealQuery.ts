9ebffc28fb67dfe4f3152463e882305ad8580dcb
Karl Gerhards <67832062+DraqzzIQ@users.noreply.github.com>
2025-07-14 15:08:30 +0200

    remove debug leftovers

--- db/queries/mealQuery.ts
+++ db/queries/mealQuery.ts
@@ -1,9 +1,9 @@
 import { db } from "~/db/client";
-import { eq, sql } from "drizzle-orm";
-import { foods, meals } from "~/db/schema";
+import { eq } from "drizzle-orm";
+import { meals } from "~/db/schema";
 
 export function mealQuery(mealId: number) {
-  const food = db.query.meals.findFirst({
+  return db.query.meals.findFirst({
     columns: {
       foodId: false,
     },
@@ -12,21 +12,6 @@ export function mealQuery(mealId: number) {
       food: true,
     },
   });
-
-  // (async () => {
-  //   const foodResolved = await food;
-  //   const rows: { vitaminA: number; name: string }[] = await db
-  //     .select({ vitaminA: foods.vitaminA, name: foods.name })
-  //     .from(foods);
-  //   console.log("test");
-  //   console.log(
-  //     await db.$client.getFirstAsync(`select vitamin_a, name from foods`),
-  //   );
-  //   console.log("test2");
-  //   // console.log(await db.query.foods.findMany());
-  // })();
-
-  return food;
 }
 
 export type MealQueryType = Awaited<ReturnType<typeof mealQuery>>;
